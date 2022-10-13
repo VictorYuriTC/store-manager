3f7db7eb52933caee30cc67558d99f89b0505c7b
Victor Yuri <victoryuritc@yahoo.com>
2022-10-13 17:24:30 -0300

    ADD productsColler, service and router

--- src/app.js
+++ src/app.js
@@ -2,10 +2,14 @@ const express = require('express');
 
 const app = express();
 
+const productsRouter = require('./routers/products.router');
+
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.use('/products', productsRouter.router);
 
 // não remova essa exportação, é para o avaliador funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima
