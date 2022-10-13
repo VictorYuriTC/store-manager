8f134db5bdcec7eb9cab14c525a73e49b293f195
Victor Yuri <victoryuritc@yahoo.com>
2022-10-13 16:03:03 -0300

    ADD getAllProducts function at productsModel

---
+++ src/models/productsModel.js
@@ -0,0 +1,12 @@
+const connection = require('./connection');
+
+const getAllProducts = async () => {
+  const [result] = await connection.execute(
+    'SELECT * FROM products',
+  );
+  return result;
+};
+
+module.exports = {
+  getAllProducts,
+};
