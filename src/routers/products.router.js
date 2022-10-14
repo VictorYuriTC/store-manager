fcb6dd3d9e0025cf52e1ada0a6a50be0fbb88bc0
Victor Yuri <victoryuritc@yahoo.com>
2022-10-13 22:10:44 -0300

    ADD validateNewProductBody at middlewares

--- src/routers/products.router.js
+++ src/routers/products.router.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const productsController = require('../controllers/products.controller');
+const productsValidation = require('../middlewares/products.validation');
 
 const router = express.Router();
 
 router.get('/', productsController.getAllProducts);
 router.get('/:id', productsController.findProductById);
-router.post('/', productsController.addNewProduct);
+router.post('/', productsValidation.validateNewProductBody, productsController.addNewProduct);
 
 module.exports = {
   router,
